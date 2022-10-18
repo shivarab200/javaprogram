39ad4c78e93a4293d69d124bc9ffa462cc82b204
shivarab200 <75974706+shivarab200@users.noreply.github.com>
2022-10-18 14:45:04 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) 
+	{
+		System.out.print("Hello world"+" ");
+		System.out.print("I'm Siva"+" ");
+		System.out.print("I'm From trichy"+" ");
+		System.out.print("Completed B.E Computer Science Engineering"+" ");
+	}
+
+} 
