4e4e84b9f82884999ffa5beabe751a1229d9d7ac
shivarab200 <75974706+shivarab200@users.noreply.github.com>
2022-10-18 15:40:47 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,34 @@
+package datatypes;
+
+public class DataTypes 
+{
+	public static void main(String[] args)
+	{
+		int intval;
+		long longval;
+		float flval;
+		double dval;
+		boolean boolval;
+		short shval;
+		byte byval;
+		char cval;
+		
+		intval = 542;
+		longval = 2261l;
+		flval = 123.1f;
+		dval = 122.12;
+		boolval = true;
+		shval = 12;
+		byval =2;
+		cval ='s';
+		System.out.println("The value of int"+" "+intval);
+		System.out.println("The value of long"+" "+longval);
+		System.out.println("The value of float"+" "+flval);
+		System.out.println("The value of double"+" "+dval);
+		System.out.println("The value of boolean"+" "+boolval);
+		System.out.println("The value of short"+" "+shval);
+		System.out.println("The value of byte"+" "+byval);
+		System.out.println("The value of char"+" "+cval);
+		
+	}
+}
